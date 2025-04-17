6da88c527f7c0e3ab5656ffa66ac70aa15b0fbff
Kevin-AppPay <kevin@apppay.tech>
2025-04-17 21:03:39 +0800

    baseline for navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,68 @@
 import React from "react";
-import { Nav } from "@/components/ds";
 import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle
+} from "@/components/ui/navigation-menu";
 
 function NavBar() {
   return (
-    <Nav>
-      <div>Logo</div>
-      <ul>
-        <li>
-          <Link href={"/"}/>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-      </ul>
-    </Nav>
+    <nav className="bg-black">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <Link href="/" className="text-white">
+                Logo
+              </Link>
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-4 flex items-center space-x-4">
+              <NavigationMenu>
+                <NavigationMenuList>
+
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger>
+                      Item One
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <NavigationMenuLink>
+                        Link
+                      </NavigationMenuLink>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger>
+                      Item Two
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <NavigationMenuLink>
+                        Link
+                      </NavigationMenuLink>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+
+                  <NavigationMenuItem>
+                    <Link href={"/contact-us"} legacyBehavior passHref>
+                      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        Docs
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
 
