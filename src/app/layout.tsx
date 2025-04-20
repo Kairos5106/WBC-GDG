969710c2d8973fa61d77e61a0de3ae32849f63c7
Kairos5106 <kevinhotzewan@gmail.com>
2025-04-20 14:40:56 +0800

    added color palette file that works with tailwind utility classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./color-palette.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
